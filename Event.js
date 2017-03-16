56b512a922097dfb952b489103087cb705259cb3
Bojana Drangova <bojana@localhost.localdomain>
2017-03-16 22:28:30 +0100

    added event.js

---
+++ Event.js
@@ -0,0 +1,8 @@
+class Event { //id, title, a description and a date
+  constructor (id, title, description, date){
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.date = date;
+  }
+}
