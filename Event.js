b846979ff6cd991b91371636510f2c6f1339295f
Bojana Drangova <bdrangova@gmail.com>
2017-03-20 14:35:06 +0100

    Delete Event.js

--- Event.js
+++
@@ -1,8 +0,0 @@
-class Event { //id, title, a description and a date
-  constructor (id, title, description, date){
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.date = date;
-  }
-}
